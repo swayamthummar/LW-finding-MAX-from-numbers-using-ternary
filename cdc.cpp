829df6d9d956cc9534fb4866f4cae0491c50e4dd
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-03-21 17:57:14 +0530

    Add files via upload

---
+++ cdc.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+    int a,b,c,d,e;     
+     
+	printf("Enter the value for A ->");
+	scanf("%d",&a);
+	printf("Enter the value for B ->");
+	scanf("%d",&b);
+	printf("Enter the value for C ->");
+	scanf("%d",&c);
+	printf("Enter the value for D ->");
+	scanf("%d",&d);
+	printf("Enter the value of  E ->");
+	scanf("%d",&e);
+	
+	printf("                                                \n");
+	
+	(a>b)? (a>c)? (a>d)? (a>e)?  printf("A is MAX") : printf("E is MAX")
+         :                      
+		 : (e>d)? printf("E is MAX") : printf("D is MAX")
+		 (d>e)? printf("D is MAX"):printf("E is MAX")
+         : (b>c)? (b>d)? (b>e)? printf("B is MAX") :  printf("E is MAX")      
+                : (c>d)?  printf("C is MAX") : printf("D is MAX")         
+                : (d>e)?  printf("E is MAX") : printf("D is MAX"); 	
+                (c>e)? printf(" is MAX"):printf("E is MAX")
+}
